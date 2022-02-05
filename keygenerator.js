dce43f3f1852b77aeadc3b803adb810d5f520e5f
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2022-02-05 14:40:28 +0530

    IDE updates

--- keygenerator.js
+++ keygenerator.js
@@ -1,11 +1,13 @@
-const EC = require('elliptic').ec;
+const EC = require("elliptic").ec;
 // elliptic curve - used in bitcoin as well
-const ec = new EC('secp256k1');
+const ec = new EC("secp256k1");
 
 const key = ec.genKeyPair();
-const publicKey = key.getPublic('hex');
-const privateKey = key.getPrivate('hex');
+const publicKey = key.getPublic("hex");
+const privateKey = key.getPrivate("hex");
 
 console.log("\nPrivate key: " + privateKey);
 
 console.log("\nPublic key: " + publicKey);
+
+// test
